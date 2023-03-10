277ce48b9d6814e53bd001ff4db4ab0c671db0cd
Kamil Kalbarczyk <k.kalbarczyk1@gmail.com>
2023-03-10 06:19:58 +0100

    add author name into footer

--- src/footer/index.js
+++ src/footer/index.js
@@ -1,14 +1,28 @@
 import { Box } from "@mui/material";
 import { Typography } from "@mui/material";
 import Divider from "@mui/material/Divider";
+import FavoriteBorderIcon from "@mui/icons-material/FavoriteBorder";
+import styled from "styled-components";
+
+const AuthorBox = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+`;
 
 export const Footer = () => {
   return (
     <Box sx={{ width: "100%", mt: "20px" }}>
       <Divider />
-      <Typography variant="subtitle2" align="center" mt={"10px"}>
-        Stopka
-      </Typography>
+      <AuthorBox>
+        <Typography variant="subtitle2" align="center" mt={"10px"}>
+          Made with
+        </Typography>
+        <FavoriteBorderIcon color="error" sx={{ m: "0 2px" }} />
+        <Typography variant="subtitle2" align="center" mt={"10px"}>
+          by Kamil Kalbarczyk
+        </Typography>
+      </AuthorBox>
     </Box>
   );
 };
